4a2085ba78071d6c68954fe7d7138c067dec78f9
Guilherme Hayashi <guilhermehayashi@Guilhermes-MacBook-Pro.local>
2018-03-24 19:17:55 -0300

    Initial Project commit

---
+++ app/src/main/java/com/example/guilhermehayashi/maonamassabasico1/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.guilhermehayashi.maonamassabasico1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /*
+    *
+    * O nome desse método deve ser o mesmo
+    *
+    * */
+    fun click(view: View) {
+
+    }
+
+}
