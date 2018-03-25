835fabadb04af1ddc73f376258c7433592bdf1bf
Guilherme Hayashi <guilhermehayashi@Guilhermes-MacBook-Pro.local>
2018-03-24 21:14:42 -0300

    Primeira Aula
    - Iniciamos o projeto com hello world.
    - Mostramos o LinearLayout: horizontal e vertical.
    - Explicamos que todas as views possuem tamanho e posição.
    - Conceito de <tag></tag> dentro dos arquivos de views.
    - Activity é onde programamos o comportamento da view. Interação do usuário com o celular.
    - Views precisam de ids para serem referenciadas na Activity.
    = Fizemos o botão ser apertado e mudar o texto.

--- app/src/main/java/com/example/guilhermehayashi/maonamassabasico1/MainActivity.kt
+++ app/src/main/java/com/example/guilhermehayashi/maonamassabasico1/MainActivity.kt
@@ -3,21 +3,37 @@ package com.example.guilhermehayashi.maonamassabasico1
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    /*
+    *
+    * LifeCycle da Activity: onPause, onResume, onCreate
+    *
+    * */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        botaoVermelho.setOnClickListener({
+            click()
+        })
     }
 
     /*
     *
-    * O nome desse método deve ser o mesmo
+    * Um método possui o seguinte formato
+    *
+    * fun nomeDoMetodo(parametros: tipo ou simplesmente vazio) {
+    *   // instruções de código que podem ser invocados a qualquer instante dentro dessa Activity
+    * }
     *
     * */
-    fun click(view: View) {
 
+    fun click() {
+        textoClicou.text = "O botão foi clicado"
     }
 
 }
